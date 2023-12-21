aa1a25d6430ecaa4b205292e92795993d33cc55a
ajsams <ajsams@me.com>
2023-12-21 15:38:05 -0500

    Create save_load.gd

---
+++ scripts/globals/save_load.gd
@@ -0,0 +1,6 @@
+extends Node
+
+var password: String = "12345"  # Password for file encryption and decryption
+
+# Objects and Paths
+var s_file = FileAccess # File object opened by the FileAccess library
