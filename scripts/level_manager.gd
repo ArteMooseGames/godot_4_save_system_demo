ccac16b810361fcf3af31c996626c7878c665d73
ajsams <ajsams@me.com>
2023-12-22 10:07:12 -0500

    Update level manager notes

--- scripts/level_manager.gd
+++ scripts/level_manager.gd
@@ -21,22 +21,25 @@ func _on_player_died():
 
 # Game will be relatively simple
 # Scenes
-# 	Levels (1 & 2)
-# 	Player
-# 	Coins (2 types)
-# 	Hazards (1 type) lots of these so the point is to move around and get coins without dying
-# 	End portal (takes you to next level, or success screen)
-# 	HUD (Shows lives and currency)
-# 	Pause menu (Reset game, Quit to title)
-# 	Title (Start Game, Credits)
-# 	You won! screen.
+# 	[ ] Levels (1 & 2, maybe more?)
+# 	[ ] Player
+# 	[ ] Coins (2 types)
+# 	[ ] Hazards (1 type) lots of these so the point is to move around and get coins without dying
+# 	[ ] End portal/door (takes you to next level, or success screen)
+# 	[ ] HUD (Shows current level, lives, and currency)
+# 	[ ] Pause menu (Reset game, Quit to title)
+# 	[ ] Title (Start Game, Credits)
+# 	[ ] You won! screen.
 
-# Track
-# 	Player Lives
-# 	Coin counts
+# Track in globals/save files
+# 	[ ] Player Lives
+# 	[ ] Coin counts
+# 	[ ] Current level
+# 	[ ] Player position
 
 # Save system will implement
 # 	What level was a player last in? 
 # 	What is the currency count?
 # 	How many lives does a player have left?
+# 	What was the last position of the player
 
